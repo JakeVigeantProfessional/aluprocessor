ecbb1db398dcffa397ad3c2698d55df6e5551e33
Jake Vigeant <108590230+JakeVigeantProfessional@users.noreply.github.com>
2023-03-06 21:27:37 -0500

    test

--- two_bit_right_shift.v
+++ two_bit_right_shift.v
@@ -1,7 +1,7 @@
 module two_bit_right_shift(data, out);
-    input [31:0] x;
+    input [31:0] data;
     output [31:0] out;
-    assign out[29:0] = x[31:2];
-    assign out[30] = x[31] ? 1'b1 : 1'b0;
-    assign out[31] = x[31];
+    assign out[29:0] = data[31:2];
+    assign out[30] = data[31] ? 1'b1 : 1'b0;
+    assign out[31] = data[31];
 endmodule
